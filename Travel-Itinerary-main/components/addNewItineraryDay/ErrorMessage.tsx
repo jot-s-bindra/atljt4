4fafa92d757e943d3f73d2b8b0e4be04e070f89d
jot-s-bindra <112833146+jot-s-bindra@users.noreply.github.com>
2024-09-20 20:51:35 +0530

    cm

--- Travel-Itinerary-main/components/addNewItineraryDay/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-
-const ErrorMessage = ({error, isTouched}: {error: any | undefined; isTouched: boolean}) => {
-  return (
-    error?.message &&
-    isTouched && <p className="text-sm font-thin text-red-400">{error?.message}</p>
-  );
-};
-
-export default ErrorMessage;
